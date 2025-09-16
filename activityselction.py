0250bbcf8b1e90766ae5800f49a1c8b5a94d4392
Harsh Trivedi <152944798+Harshtrivedi456@users.noreply.github.com>
2025-09-16 20:43:32 +0530

    Add files via upload

---
+++ activityselction.py
@@ -0,0 +1,18 @@
+# Activity Selection Problem (Greedy Optimal)
+
+def activity_selection(start, finish):
+    activities = sorted(zip(start, finish), key=lambda x: x[1])
+    chosen = []
+    last_finish = -1
+    for s, f in activities:
+        if s >= last_finish:
+            chosen.append((s, f))
+            last_finish = f
+    return chosen
+
+
+# Example
+start = [1, 3, 0, 5, 8, 5]
+finish = [2, 4, 6, 7, 9, 9]
+selected = activity_selection(start, finish)
+print("Selected Activities (start, finish):", selected)
