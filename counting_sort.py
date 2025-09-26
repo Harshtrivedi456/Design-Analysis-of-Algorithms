7a92a0af4e3f81138789c2f392bed37601c76b06
Harsh Trivedi <152944798+Harshtrivedi456@users.noreply.github.com>
2025-09-26 18:56:58 +0530

    Add files via upload

---
+++ counting_sort.py
@@ -0,0 +1,19 @@
+def counting_sort(arr):
+    if not arr:
+        return []
+
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+
+    for num in arr:
+        count[num] += 1
+
+    sorted_arr = []
+    for i in range(len(count)):
+        sorted_arr.extend([i] * count[i])
+
+    return sorted_arr
+
+arr = [4, 2, 2, 8, 3, 3, 1]
+sorted_arr = counting_sort(arr)
+print("Sorted Array:", sorted_arr)
